1a9efe6c1d31d5593fdc36a0b6dda2c024528110
abmemon <inbox@abmemon.com>
2024-03-15 10:33:20 +0500

    react-router-dom for routes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import "./assets/css/frontend.css"
 // import Header from './components/frontend/header/Header';
 // import Content from './components/frontend/content/Content';
@@ -5,6 +6,7 @@ import "./assets/css/frontend.css"
 // import Footer from './components/frontend/footer/Footer';
 import Product from "./components/frontend/content/Product";
 import ProductDetails from "./components/frontend/content/ProductDetails";
+import Content from "./components/frontend/content/Content";
 
 function App() {
   return (
@@ -18,6 +20,18 @@ function App() {
       {/** FOOTER START HERE  */ }
       {/* <Footer /> */}
       {/* <Arrays /> */}
+      <BrowserRouter>
+      <ul>
+        <li><Link to="/"> Home</Link></li>
+        <li><Link to="/content"> Content</Link></li>
+      </ul>
+
+      <Routes>
+       <Route path="/" element={<Product />}/>
+       <Route path="/content" element={<Content />}/>
+      </Routes>
+      
+      </BrowserRouter>
       <Product />
       <ProductDetails />
     </div>
